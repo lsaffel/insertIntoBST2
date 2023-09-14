0fc856951a3851b35cf451e723f81573feda9cf5
Lisa Saffel <58794458+lsaffel@users.noreply.github.com>
2023-09-13 22:07:58 -0600

    removed duplicated line new_note, put it at top instead

--- insertIntoBST2.py
+++ insertIntoBST2.py
@@ -6,21 +6,18 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        # the code and class above this line was provided in the problem
+        new_node = TreeNode(val)
         if root is None:
-            new_node = TreeNode(val)
             root = new_node
             return root
 
         if val < root.val:
             if root.left is None:
-                new_node = TreeNode(val)
                 root.left = new_node
             else:
                 self.insertIntoBST(root.left, val)
         else:  # val > root.val
             if root.right is None:
-                new_node = TreeNode(val)
                 root.right = new_node
             else:
                 self.insertIntoBST(root.right, val)
